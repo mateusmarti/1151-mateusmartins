353a28a9025b7082974e03db855ab93098e7b602
CARDOSOCRIS <allanis.ribeiro.estudante@eteot.rio.br>
2021-11-10 15:34:56 -0300

    exercicio 8

---
+++ javascript/ex008/script.js
@@ -0,0 +1,28 @@
+let saida = document.getElementById("saida")
+let titulo = document.getElementById("titulo")
+let mensagem = document.getElementById("mensagem")
+
+saida.style.visibility = "hidden"
+
+function cadastro() {
+    alert("certo")
+    let total =  Number(document.getElementById("total").value)
+
+    let idade = 0
+    let soma = 0
+    let media = 0
+
+    let cont = 1
+    while (cont <= total) {
+        idade = Number(prompt(`Idade da ${cont}a pessoa`))
+        soma += idade
+        cont ++
+    }
+
+    media = soma / total
+    titulo.innerHTML = `${total} pessoas cadastradas`
+    mensagem.innerHTML = `No grupo de ${total} pessoas cadastradas, a média de idade é de <strong>${media.toFixed(1)} anos.</strong>`
+
+    saida.style.visibility = "visible"
+    return false
+}
