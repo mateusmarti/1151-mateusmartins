10067bb8819164ba7edb0fdaf6f7995c592ca75b
Jhoel Webert <barbara.morais.estudante@eteot.rio.br>
2021-11-17 15:54:58 -0300

    17/11/21

---
+++ javascript/ex013/script.js
@@ -0,0 +1,31 @@
+let resp = document.getElementById("res")
+
+        function mudanum() { 
+            let btn = document.getElementById("btnfat")
+            let num = Number(document.getElementById("num").value)
+            btn.value = `calcular ${num}`
+    }
+        function fatorial() {
+            let num = Number(document.getElementById("num").value)
+            if (num == 0 || num == 1) {
+                resp.innerText = `${num}! = 1`
+            } else {
+                let cont = num
+                let fat = 1 
+                resp.innerText = `${num}! = `
+                while (cont >= 1) {
+                    fat *= cont
+                    resp.innerText += `${cont}`
+                    if (cont == 1) {
+                        resp.innerText += " = "
+                    } else {
+                        resp.innerText += " x "
+                    }
+                    cont --  
+                }
+                resp.innerText += `${fat}`
+            }
+
+            return false
+        }
+    
