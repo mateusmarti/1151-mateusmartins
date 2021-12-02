52e5448ef778113e4cf31305558abadda1c03462
Gustavocassaro <91631720+Gustavocassaro@users.noreply.github.com>
2021-12-02 15:19:56 -0300

    02/12/2021

---
+++ javascript/ex001/script.js
@@ -0,0 +1,15 @@
+let res = document.getElementById("res")
+function veja() {
+    let nome = document.getElementById("name").value
+    let ano = Number(document.getElementById("idd").value)
+    let idade = 2021 - ano
+    
+    if (idade <16) {
+        res.innerHTML = `Atenção ${nome}, como você tem ${idade} anos de idade, <strong>VOCÊ NÃO PODE VOTAR!</strong>`
+    } else if (idade <18) {
+        res.innerHTML = `Atenção ${nome}, como você tem ${idade} anos de idade, <strong>VOCÊ PODE VOTAR, MAS NÃO TEM OBRIGAÇÃO!</strong>`
+    } else {
+        res.innerHTML = `Atenção ${nome}, como você tem ${idade} anos de idade, <strong>VOCÊ TEM A OBRIGAÇÃO DE VOTAR!</strong>`
+    }
+    return false
+}
