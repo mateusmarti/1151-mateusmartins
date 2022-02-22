f07cd33b3af13de0e475926ea2e285580810524f
Mateus <joao.alves.estudante@eteot.rio.br>
2022-02-22 15:22:12 -0300

    22/2/22

--- sis-web/ex000/js/script.js
+++ sis-web/ex000/js/script.js
@@ -1,3 +1,6 @@
-function trocar(caminho) {
-    document.getElementById('imgprin').src = caminho
+function trocar(caminho,nome,legenda) {
+    document.getElementById('imgprin').src = caminho;
+    document.getElementById('titulo').value = nome;
+    document.getElementById('leg').value = legenda;
+
 }
